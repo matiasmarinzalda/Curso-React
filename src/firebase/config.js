b2714a4c9f7311fd1affe07f0ae44b4f366e1923
matiasmarinzalda <matias.marinzalda@gmail.com>
2026-06-22 08:38:25 -0300

    login arreglado export

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -18,9 +18,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+ const db = getFirestore(app);
 
 //para cuando veamos el login
-export const auth = getAuth(app);
+ const auth = getAuth(app);
 
 export { db, auth };
